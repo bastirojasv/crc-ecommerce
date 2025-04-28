e27cf98fd619a1923d2f984a71d62cf96cf21a05
bastirojasv <basti.rojasv@gmail.com>
2025-04-27 21:00:05 -0400

    refactor: update comments in server.js for clarity and consistency; adjust package.json dependencies

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Servir los archivos estáticos del dist/
+// Servir archivos estáticos del build de Angular
 app.use(express.static(path.join(__dirname, 'dist/crc-ecommerce')));
 
-// Redirigir todas las peticiones a index.html
+// Cualquier otra ruta debe devolver index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/crc-ecommerce/index.html'));
 });
 
-// Puerto
+// Escuchar el puerto que asigna Heroku o localmente en 8080
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Servidor iniciado en el puerto ${PORT}`);
+  console.log(`Servidor iniciado en puerto ${PORT}`);
 });
